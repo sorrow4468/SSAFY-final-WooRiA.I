6cceacecb98e3b29487bc9b5d198e24077d1397b
bandong <bandong92@gmail.com>
2022-04-28 16:04:33 +0900

    [ai] feat: add Client 기본기능

---
+++ AI/CCTV/CcTv.py
@@ -0,0 +1,11 @@
+import cv2
+
+class CcTv():
+    def rtsp():
+        cam = cv2.VideoCapture(
+            'rtsp://tapoadmin:tapoadmin@bandong.iptime.org:554/stream1')
+        return cam
+        # while True:
+        #     _, img = cam.read()
+        #     cv2.imshow("TEST", img)
+        #     cv2.waitKey(1)
