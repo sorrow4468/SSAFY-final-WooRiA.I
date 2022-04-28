82b451b6788d5ccc1b065a9ca66520e0811471d7
이우철 <dncjf1969@naver.com>
2022-04-28 17:06:02 +0900

    [front] add: 로그인 / 회원가입 / 비밀번호 수정 페이지 추가

--- frontend/src/pages/auth/recover-password/RecoverPassword.vue
+++ frontend/src/pages/auth/recover-password/RecoverPassword.vue
@@ -1,41 +1,70 @@
 <template>
   <form @submit.prevent="onsubmit" class="login">
-    <div class="row mb-2">
-      <!-- <va-input
+    <div class="mb-5">
+      <!-- 아이디 -->
+      <va-input
+        class="mb-3"
+        v-model="id"
+        type="id"
+        :label="$t('auth.id')"
+        :error="!!emailErrors.length"
+        :error-messages="idErrors"
+      />
+      <!-- 이메일 -->
+      <va-input
         class="mb-3"
         v-model="email"
         type="email"
         :label="$t('auth.email')"
         :error="!!emailErrors.length"
         :error-messages="emailErrors"
-      /> -->
+      />
+      <!-- 전화번호 -->
+      <va-input
+        class="mb-3"
+        v-model="phone_number"
+        type="phone_number"
+        :label="$t('auth.phone_number')"
+        :error="!!emailErrors.length"
+        :error-messages="phoneNumberErrors"
+      />
     </div>
     <div class="row justify--center">
-      <!-- <va-button type="submit" class="my-0">{{ $t('auth.reset_password') }}</va-button> -->
+      <va-button type="submit" class="my-0">{{
+        $t("auth.reset_password")
+      }}</va-button>
     </div>
   </form>
 </template>
 
 <script>
 export default {
-  name: 'recover-password',
-  data () {
+  name: "recover-password",
+  data() {
     return {
-      email: '',
+      id: "",
+      email: "",
+      phoneNumber: "",
+      idErrors: [],
       emailErrors: [],
-    }
+      phoneNumberErrors: []
+    };
   },
   methods: {
-    onsubmit () {
-      if (!this.email) {
-        this.emailErrors = ['Email is required']
-      } else {
-        this.$router.push('/')
-      }
-    },
-  },
-}
+    onsubmit() {
+      this.idErrors = this.id ? [] : ["아이디를 적어주세요"];
+      this.emailErrors = this.email ? [] : ["이메일을 적어주세요"];
+      this.phoneNumberErrors = this.phoneNumber
+        ? []
+        : ["전화번호를 적어주세요"];
+      // if (!this.email) {
+      //   this.emailErrors = ["이메일을 적어주세요"];
+      // } else {
+      //   this.$router.push("/");
+      // }
+    }
+  }
+};
 </script>
 
-<style lang="scss">
-</style>
+<style lang="scss"></style>
