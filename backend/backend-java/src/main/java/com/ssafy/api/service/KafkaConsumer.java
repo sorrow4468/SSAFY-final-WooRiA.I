fe78e731d7be89be5807dc30ca723593a926f0fe
sky6min <sky6min@gmail.com>
2022-04-28 11:53:43 +0900

    [back] feat : kafka connection and system prototype

--- backend/backend-java/src/main/java/com/ssafy/api/service/KafkaConsumer.java
+++ backend/backend-java/src/main/java/com/ssafy/api/service/KafkaConsumer.java
@@ -1,5 +1,8 @@
 package com.ssafy.api.service;
 
+import com.ssafy.api.request.SocketVO;
+import com.ssafy.common.websocket.WebSocket;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -8,8 +11,13 @@ import java.io.IOException;
 @Service
 public class KafkaConsumer {
 
+    @Autowired
+    private WebSocket webSocket;
+
     @KafkaListener(topics = "kafka-demo2", groupId = "kafka-demo")
     public void consume(String message) throws IOException {
         System.out.println(String.format("Consumed message : %s", message));
+
+        webSocket.sendAllMessage(message);
     }
 }
