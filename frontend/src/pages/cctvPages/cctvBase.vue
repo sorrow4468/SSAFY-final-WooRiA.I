f8ca445d2022760dde77669ea14d05fae25fdbc8
손한기 <wwenxt@naver.com>
2022-05-02 15:43:39 +0900

    update : sidebar 수정 및 cctv 페이지 생성

---
+++ frontend/src/pages/cctvPages/cctvBase.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>hi</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "cctvbase",
+  data() {
+    return {
+    };
+  },
+  computed: {
+  },
+  methods: {
+  }
+};
+</script>
