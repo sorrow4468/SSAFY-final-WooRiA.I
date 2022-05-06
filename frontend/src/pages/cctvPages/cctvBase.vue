81c46c64484cba631c060e32c74796c02445bb4d
손한기 <wwenxt@naver.com>
2022-05-06 11:38:11 +0900

    update : timer, clock, screen component 완성

--- frontend/src/pages/cctvPages/cctvBase.vue
+++ frontend/src/pages/cctvPages/cctvBase.vue
@@ -1,13 +1,29 @@
 <template>
-  <div>
-    <p>hi</p>
+  <div  class="pageSet">
+    <div class="flex xs8 md8">
+      <cctvscreen/>
+    </div>
+    <div class="flex xs4 md4">
+      <cctvclock color="green"/>
+      <cctvtimer/>
+    </div>
   </div>
 </template>
 
 <script>
+import cctvclock from './cctvClock.vue'
+import cctvtimer from './cctvTimer.vue'
+import cctvscreen from './cctvScreen.vue'
 
-export default {
+export default {  
   name: "cctvbase",
+  components: {
+    cctvclock,
+    cctvtimer,
+    cctvscreen
+
+  },
+
   data() {
     return {
     };
@@ -18,3 +34,9 @@ export default {
   }
 };
 </script>
+<style>
+.pageSet {
+  display: flex;
+  flex-direction: row;
+}
+</style>
