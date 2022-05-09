bc156cf223b7ffcdd7508db4d6b7bfe69c036a8d
sky6min <sky6min@gmail.com>
2022-05-09 14:37:34 +0900

    [back] feat : update corsconfg 2

--- backend/backend-java/src/main/java/com/ssafy/config/WebMvcConfig.java
+++ backend/backend-java/src/main/java/com/ssafy/config/WebMvcConfig.java
@@ -24,7 +24,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "https://xn--vk1bw3clxiimaf76b.kr/api_be")
+                .allowedOrigins("http://localhost:8080", "https://xn--vk1bw3clxiimaf76b.kr")
                 .allowCredentials(true)
                 .allowedHeaders("*")
                 .allowedMethods("*")
