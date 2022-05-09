d39d325fae77142542bf433758ffc4378ba72db1
손한기 <wwenxt@naver.com>
2022-05-09 15:22:29 +0900

    hotdog

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -35,12 +35,12 @@ import Vue from 'vue';
             },
             initWebSocket: function () {
                 // WebSocket is different from ordinary requests in terms of protocol, WS is equivalent to http, WSS is equivalent to HTTPS
-                this.websock = new WebSocket("ws://13.125.56.138:8971/websocket/DPS007");
+                this.websock = new WebSocket("wss://13.125.56.138:8971/websocket/DPS007");
                 this.websock.onopen = this.websocketonopen;
                 this.websock.onerror = this.websocketonerror;
                 this.websock.onmessage = this.websocketonmessage;
                 this.websock.onclose = this.websocketclose;
-              },
+              },  
               websocketonopen: function () {
                 console. log ("WebSocket Connection Successful");
               },
