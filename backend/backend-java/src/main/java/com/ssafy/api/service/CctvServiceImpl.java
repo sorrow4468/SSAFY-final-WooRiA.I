7da3ed2a45d9beece4e521deeb281444faf252b1
sky6min <sky6min@gmail.com>
2022-05-10 11:42:18 +0900

    [back] feat : add cctvInfo Repository

--- backend/backend-java/src/main/java/com/ssafy/api/service/CctvServiceImpl.java
+++ backend/backend-java/src/main/java/com/ssafy/api/service/CctvServiceImpl.java
@@ -1,7 +1,10 @@
 package com.ssafy.api.service;
 
 import com.ssafy.api.request.KafkaVO;
+import com.ssafy.db.entity.CCTV;
+import com.ssafy.db.entity.Danger;
 import com.ssafy.db.entity.User;
+import com.ssafy.db.repository.CCTVRepository;
 import com.ssafy.db.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,6 +18,9 @@ public class CctvServiceImpl implements CctvService {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    CCTVRepository cctvRepository;
+
     @Override
     public List<User> getUserList(LocalDateTime curTime) {
 
@@ -24,8 +30,36 @@ public class CctvServiceImpl implements CctvService {
 
     @Override
     public void saveVideo(KafkaVO kafkaVO) {
+        String location;
+        if(kafkaVO.getCameraNumber().equals("1")) {
+            location = "신호동" +
+                    "234-6" + "소정공원";
+        }else if(kafkaVO.getCameraNumber().equals("2")) {
+            location = "명지동" +
+                    "3230-12" + "링컨공원";
+        }else if(kafkaVO.getCameraNumber().equals("3")) {
+            location = "범방동" +
+                    "1875-4" + "금병공원";
+        }else {
+            location = "명지동" +
+                    "3428-4" + "나뭇잎 공원";
+        }
 
-
+        if(kafkaVO.getDetection().equals("1")) {
+            CCTV cctv = new CCTV().builder()
+                    .VIDEO_URL(kafkaVO.getUrl())
+                    .type(kafkaVO.getCameraNumber())
+                    .LOCATION(location)
+                    .danger(Danger.VIOLENCE).build();
+            cctvRepository.saveAndFlush(cctv);
+        }else {
+            CCTV cctv = new CCTV().builder()
+                    .VIDEO_URL(kafkaVO.getUrl())
+                    .type(kafkaVO.getCameraNumber())
+                    .LOCATION(location)
+                    .danger(Danger.STUMBLE).build();
+            cctvRepository.saveAndFlush(cctv);
+        }
 
     }
 }
